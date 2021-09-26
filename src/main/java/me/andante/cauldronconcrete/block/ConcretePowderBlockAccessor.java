70017de9bb87ecf062444fd9cc1e78ffe41f77ef
PotatoPresident <potatoboy9999@outlook.com>
2021-09-26 14:34:35 -0700

    Update to 1.17.1 Optimize code to not run every tick on all items Added particles and sounds

--- src/main/java/me/andante/cauldronconcrete/block/ConcretePowderBlockAccessor.java
+++
@@ -1,9 +0,0 @@
-package me.andante.cauldronconcrete.block;
-
-import net.minecraft.block.Block;
-import org.jetbrains.annotations.NotNull;
-
-public interface ConcretePowderBlockAccessor {
-    @NotNull
-    Block cauldronconcrete_getBlock();
-}
